568076bfe1eafc094beb8400db427e6f82b6900f
mert dogan <trsverige466@gmail.com>
2022-01-21 12:58:07 +0300

    first

---
+++ src/test/java/day05_MavenProjeOusturma/C01_FirstMavenClass.java
@@ -0,0 +1,26 @@
+package day05_MavenProjeOusturma;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_FirstMavenClass {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //1- https://www.amazon.com/ sayfasina gidelim
+        driver.get("https://www.amazon.com/");
+        //2- arama kutusunu locate edelim
+        //3- “Samsung headphones” ile arama yapalim
+        //4- Bulunan sonuc sayisini yazdiralim
+        //5- Ilk urunu tiklayalim
+        //6- Sayfadaki tum basliklari yazdiralim
+    }
+}
