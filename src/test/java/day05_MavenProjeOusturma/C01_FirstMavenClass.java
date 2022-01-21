72992daf46faf8d2f5c8abd09b4be1b10d5e3e59
mert dogan <trsverige466@gmail.com>
2022-01-21 13:06:15 +0300

    firstt

--- src/test/java/day05_MavenProjeOusturma/C01_FirstMavenClass.java
+++ src/test/java/day05_MavenProjeOusturma/C01_FirstMavenClass.java
@@ -16,11 +16,13 @@ public class C01_FirstMavenClass {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         //1- https://www.amazon.com/ sayfasina gidelim
+
         driver.get("https://www.amazon.com/");
         //2- arama kutusunu locate edelim
         //3- “Samsung headphones” ile arama yapalim
         //4- Bulunan sonuc sayisini yazdiralim
         //5- Ilk urunu tiklayalim
         //6- Sayfadaki tum basliklari yazdiralim
+        // merhaba
     }
 }
